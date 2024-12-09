dba6f1d26dfd767801893f569b097912cbc96c04
Vadim Maslov <dub4ex@mail.ru>
2024-12-09 23:56:08 +0300

    fix minor issue

--- scripts/index.js
+++ scripts/index.js
@@ -8,22 +8,26 @@
 
 // @todo: Вывести карточки на страницу
 
-function createCard(cardInfo, deleteEvent) {                              //Функция создания карточки (Принимает в себя элемент массива и функцию удаления карточки)
-  const cardTemplate = document.querySelector('#card-template').content;  //Наодим темплейт карточки и берем из него фрагмент документа и присваиваем переменную
-  const card = cardTemplate.querySelector('.card').cloneNode(true);       //Присваиваем li(карточке) переменную и клонируем её
-  const cardTitle = card.querySelector('.card__title');                   //Переменная для заголовка внутри карточки
-  const cardImage = card.querySelector('.card__image');                   //Переменная для изображения внутри карточки
-  const deleteButton = card.querySelector('.card__delete-button');        //Переменная для кнопки удаления внутри карточки
-  cardTitle.textContent = cardInfo.name;                                  //Добавляем текст в заголвок из элемента массива со значением name
-  cardImage.alt = cardInfo.name;                                          //Добавляем альт в изображение из элемента массива со значением name
-  cardImage.src = cardInfo.link;                                          //Добавляем ссылку на изображение из элемента массива со значением link
-  deleteButton.addEventListener('click', deleteEvent);                    //Добавляем на кнопку удаления слушатель
-  return card;                                                            //Возвращаем карточку со всеми нужными элементами
+function createCard(cardInfo, deleteEvent) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardTitle = card.querySelector('.card__title');
+  const cardImage = card.querySelector('.card__image');
+  const deleteButton = card.querySelector('.card__delete-button');
+  cardTitle.textContent = cardInfo.name;
+  cardImage.alt = cardInfo.name;
+  cardImage.src = cardInfo.link;
+  deleteButton.addEventListener('click', () => deleteEvent(card));
+  return card;
 }
-const cardList = document.querySelector('.places__list');                 //Находим список куда будем вставлять карточки
-initialCards.forEach(function(cardInfo){                                  //Для каждого элемента массива выполняем функцию
-  const card = createCard(cardInfo, function() {                          //Создаём карточку, добавляя ей инфу из массива и функцию удаления
-    card.remove();                                                        //Удаление карточки
-  });
-  cardList.append(card);                                                  //Добавление карточки на страницу
+
+const cardList = document.querySelector('.places__list');
+
+function deleteCard(card){
+  card.remove();
+}
+
+initialCards.forEach(function(cardInfo){
+  const card = createCard(cardInfo, deleteCard);
+  cardList.append(card);
 });
